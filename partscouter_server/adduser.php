68b355e49ec2a8eb6ad3b45104c3a4de5ab8746a
Alex Charles <104869636+alexcharles44444@users.noreply.github.com>
2022-11-30 15:45:48 -0800

    PartScouter Server Upload

---
+++ partscouter_server/adduser.php
@@ -0,0 +1,21 @@
+<?php
+
+// header("Access-Control-Allow-Origin: *");
+require "./addinvoice.php";
+global $_conn;
+$user = $_REQUEST["user"];
+$pass = $_REQUEST["pass"];
+
+$data = mysqli_query($_conn, "select * from users where user='" . $user . "' and password='" . $pass . "';");
+$assoc = $data->fetch_assoc();
+if ($assoc == null) {
+    mysqli_query($_conn, "
+                INSERT INTO users VALUES (
+                    '" . $user . "',
+                    '" . $pass . "'
+                );");
+    mysqli_query($_conn, 'create table ' . $user . ' (
+                 sent char(1),
+                 xml text(16383)
+                 );');
+}
