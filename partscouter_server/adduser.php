0afc6280f0a18ebfb0e40251d434f7f8ea58c9d3
Alex Charles <104869636+alexcharles44444@users.noreply.github.com>
2022-12-02 13:30:31 -0800

    Update adduser.php

--- partscouter_server/adduser.php
+++ partscouter_server/adduser.php
@@ -1,5 +1,9 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+
 require "./addinvoice.php";
 global $_conn;
 $user = $_REQUEST["user"];
